589af1c04ceb74f47ca78fe987eac225c1892e4e
Prajapathi Manish Kumar <pmanishkumar2006@gmail.com>
2026-07-17 15:06:57 +0530

    Created a html file and designed its functionality

---
+++ src/test/java/com/beinginfinity/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beinginfinity.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
